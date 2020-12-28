489876b3fd73dcdcd1ad873e6133e9bd74b541f7
Miroslaw Staron <miroslaw.staron@live.se>
2020-12-28 15:57:45 +0100

    Added the comment

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-// There should be a comment here!
+// This is the first program
+// which demonstrates the use of the editor
+// It is the standard, Kernighan and Ritchie's "Hello world!"
 void main(void)
 {
 	printf("Hello World!\n");
